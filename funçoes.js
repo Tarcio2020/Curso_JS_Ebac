cd618bb3001e16b3b2b160efb9caa8998d226daf
Tarcio2020 <94722456+Tarcio2020@users.noreply.github.com>
2023-05-14 00:16:06 -0300

    add_Funcoes

---
+++ funçoes.js
@@ -0,0 +1,28 @@
+let nome = prompt("Digite seu nome; ");
+
+function dezoito() {
+    alert("Olá Mundo" + nome);
+
+}
+
+dezoito()
+
+
+nomedousuario = "Tarcio Teles"
+
+
+function dezoito(nomedousuario) {
+    alert("olá" + nomedousuario)
+}
+
+let anodenascimento = 1994
+
+function calcula(anodenascimento) {
+    let idade = 2022 - anodenascimento
+}
+
+
+console.log(idade)
+
+
+
