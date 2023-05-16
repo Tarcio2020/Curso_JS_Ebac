781044b3f75c1da9c8179c3b83fd9a71d092b560
Tarcio2020 <94722456+Tarcio2020@users.noreply.github.com>
2023-05-16 18:00:53 -0300

    add_SeletoresJs

---
+++ seletores.js
@@ -0,0 +1,38 @@
+//Para acessar um elemento JS precisamos acessar
+
+//documento de formulário 
+
+document.getElementsByTagnaem('input')
+
+
+let inputs = document.getElementsByTagnaem('input');
+
+<input type="number" required = "Número da Conta"> </input>
+
+//para selecionar um elemento pelo id
+
+
+document.getElementaryId('container'):
+
+
+//selecionando um formulário e o botão 
+
+document.getElementsByTagName('forms')[0] getElementsByTagName('button');
+
+
+//Para selecionar uma classe
+
+document.getElementsByClassName('container')
+
+
+//seleção com query
+
+document.queryselector('button') //vai selecionar só um elemento
+
+document.queryselectorAll('input[type=number]') //vai selecionar tudo 
+
+
+document.body
+document.head
+
+
