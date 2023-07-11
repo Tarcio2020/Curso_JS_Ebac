2fcb13e108eb59cca022cffa24cbfe521bc9b91e
Tarcio Teles <tarcioapps7@gmail.com>
2023-07-11 20:50:18 -0300

    add

--- heranca.js
+++ heranca.js
@@ -9,7 +9,19 @@ function Pessoa(nome) {
 
 function Funcionarios(nome, cargo, salario) {
     this.cargo = cargo;
-    this.salario = salario;
+    //this.salario = salario;
+
+    //Getters e Setters
+    this.getSalario = function() {
+        return `O salario de ${nome} é ${_salario}`
+    }
+
+    this.setSalario = function(valor) {
+        if(typeof valor === 'number') {
+        _salario = valor;
+        }
+    }
+
     this.dizCargo = function() {
         console.log(this.cargo)
     }
@@ -19,7 +31,7 @@ function Funcionarios(nome, cargo, salario) {
 }
 
 const pessoa1 = new Pessoa('Maria')
-const Funcionarios1 = new Funcionarios ("Maria", 'Dev. Java', 10000);
+const Funcionarios1 = new Funcionarios ("Maria", 'Dev. Java', 155844854);
 
 console.log(pessoa1);
 console.log(Funcionarios1);
@@ -27,5 +39,19 @@ console.log(Funcionarios1);
 Funcionarios1.dizOi();
 Funcionarios1.dizCargo();
 
+//_________________Encapsulamento________________//
+
+//Abaixo coonseguimos mudar o salário pq o objeto é publico//
+//Funcionarios1.salario = 1000;
+console.log(Funcionarios1);
+
+//Para deixarmos o atributo privado passamos uma variável ao invés do this.atributo//
+//Atributo privado não é acessivel atraves do ponto....só através de uma função//
+Funcionarios1.atribuiSalario('mil');
 
 
+console.log(Funcionarios1.getSalarioSalario());
+
+
+
+;
