daf95829d5b3e35272eccb6b8d7644553fb4c166
Tarcio Teles <tarcioapps7@gmail.com>
2023-07-11 21:31:08 -0300

    add

--- heranca.js
+++ heranca.js
@@ -10,16 +10,25 @@ function Pessoa(nome) {
 function Funcionarios(nome, cargo, salario) {
     this.cargo = cargo;
     //this.salario = salario;
-
+    let _salario = salario;
     //Getters e Setters
+    //getters retorna 
+    //setters atribui 
     this.getSalario = function() {
-        return `O salario de ${nome} é ${_salario}`
+        return `O salario de ${this.nome} é ${_salario}`
     }
 
     this.setSalario = function(valor) {
         if(typeof valor === 'number') {
         _salario = valor;
         }
+    }
+
+
+
+    this.aumentoSalario = function (){
+        const aumento = _salario * 1.10;
+        console.log(aumento);
     }
 
     this.dizCargo = function() {
@@ -30,11 +39,12 @@ function Funcionarios(nome, cargo, salario) {
     Pessoa.call(this, nome);
 }
 
-const pessoa1 = new Pessoa('Maria')
 const Funcionarios1 = new Funcionarios ("Maria", 'Dev. Java', 155844854);
+const Funcionarios2 = new Funcionarios ("Tarcio", 'Dev. C#', 20000);
 
-console.log(pessoa1);
-console.log(Funcionarios1);
+
+//console.log(pessoa1);
+//console.log(Funcionarios1);
 
 Funcionarios1.dizOi();
 Funcionarios1.dizCargo();
@@ -43,15 +53,13 @@ Funcionarios1.dizCargo();
 
 //Abaixo coonseguimos mudar o salário pq o objeto é publico//
 //Funcionarios1.salario = 1000;
-console.log(Funcionarios1);
+//console.log(Funcionarios1);
 
 //Para deixarmos o atributo privado passamos uma variável ao invés do this.atributo//
 //Atributo privado não é acessivel atraves do ponto....só através de uma função//
-Funcionarios1.atribuiSalario('mil');
+Funcionarios1.setSalario(10000);
+console.log(Funcionarios1.getSalario());
+Funcionarios1.aumentoSalario();
 
-
-console.log(Funcionarios1.getSalarioSalario());
-
-
-
-;
+console.log(Funcionarios2.getSalario());
+Funcionarios2.aumentoSalario();
---
+++ main.js
@@ -0,0 +1,10 @@
+
+
+aumentoSalario = function (salario) {
+    this.salario = salario;
+    let aumento = salario * 1.10;
+    console.log(aumento)
+}
+
+
+aumentoSalario(81561561)
